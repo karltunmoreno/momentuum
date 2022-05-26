425faf6662bbaa7b581b4ce32243f659137d6d0d
unknown <Karltunmoreno@gmail.com>
2022-05-26 00:25:38 -0400

    cycles

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import coverImage from '../../assets/cover/cover-image.jpg';
 function About() {
   return (
     <section className="my-5">
-      <h1 id="about">MOMENTUUM</h1>
+      <h1 id="about">MOMENTUUM photoworks</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="my-2">
         <p>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -38,7 +38,7 @@ function ContactForm() {
 
   return (
     <section>
-      <h1 data-testid="h1tag">Contact me</h1>
+      <h1 data-testid="h1tag">Contact me. Lead Photographer. Karltun Moreno</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
